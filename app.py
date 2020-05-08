27ca4fb95092594ab846c76cbaec1e3e251cd162
v25 <>
2020-05-08 16:40:39 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, jsonify, url_for
+
+import datetime as DT
+def conv(epoch):
+    """ helper function convers epoch to iso format compatible with moment """
+    return DT.datetime.utcfromtimestamp(epoch).isoformat()
+
+sample_csv = '''2020-05-05,103
+                2020-05-06,143
+                2020-05-07,184
+                2020-05-08,244
+                2020-05-09,280
+                2020-05-10,324
+                2020-05-11,387
+                2020-05-12,491
+                2020-05-13,645
+                2020-05-14,925'''
+
+sample_list = [ {'x': conv(1588745371), 'y': 400},
+                {'x': conv(1588845371), 'y': 500},
+                {'x': conv(1588946171), 'y': 800}]
+
+def csv_to_points(data):
+    """ Convert CSV string to list of dictionaries compatible with chart data obj """
+    data = data.split('\n')
+    data = [d.split(',') for d in data]
+    return [ dict(x = d[0], y = d[1]) for d in data]
+
+app = Flask(__name__)
+
+# Disable caching of staic assets; useful for dev
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+
+@app.route('/')
+def index():
+    ''' Route which renders the chart page.  Pass the endpoint which returns jsonified data '''
+    return render_template('index.html', CHART_ENDPOINT = url_for('data'))
+
+@app.route('/data')
+def data():
+    ''' Endpoint which returns jsonified data '''
+
+    d = {'datasets':
+            [
+                {'title': 'From Dict',
+                 'data': sample_list,
+                },
+
+                {'title': 'From CSV',
+                 'data': csv_to_points(sample_csv)}
+            ],
+        }
+
+    return jsonify(d)
+
---
+++ static/flask_chart.js
@@ -0,0 +1,72 @@
+var config = {
+  type: 'line',
+  data: {
+    datasets: []
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'day'
+        },
+      },],
+      yAxes: [{
+        display: true,
+        // type: 'logarithmic',
+      }]
+    },
+    responsive: true
+  }
+};
+
+////
+
+Array.prototype.convertData = function() {
+  /// Function which convers X values to moment object
+  for (i = 0; i < this.length; i++) {
+    this[i] = {
+      'x': moment(this[i].x),
+      'y': this[i].y
+    }
+  }
+  return this;
+}
+
+function getRandomColor() {
+    // Function to generate random color for the lines.
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function create_chart(ctx, unit = 'day'){
+  config.options.scales.xAxes[0].time.unit = unit;
+  return new Chart(ctx, config);
+}
+
+function add_line(chart, endpoint) {
+  
+  var url = new URL("http://" + window.location.host + endpoint);
+  // params = {'q': q}
+  // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((resp_data) => {
+      resp_data.datasets.forEach((dataSet) => {
+        chart.data.datasets.push({
+          label: dataSet['title'],
+          data: dataSet['data'].convertData(),
+          fill: false,
+          backgroundColor: getRandomColor(),
+        })
+      });
+      chart.update();
+    });
+};
