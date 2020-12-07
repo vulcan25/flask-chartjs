bc3e4cee6bd02c0f66d7cbfea45bbbca5f619971
v25 <>
2020-12-07 20:37:55 +0000

    answer for SO q 65187462

--- app.py
+++ app.py
@@ -1,30 +1,15 @@
 from flask import Flask, render_template, jsonify, url_for, request
 
 import datetime as DT
-def conv(epoch):
+
+def conv(date_str):
     """ helper function convers epoch to iso format compatible with moment """
-    return DT.datetime.utcfromtimestamp(epoch).isoformat()
+    return DT.datetime.strptime(date_str, '%m-%d-%Y').isoformat()
 
-sample_csv = '''2020-05-05,103
-                2020-05-06,143
-                2020-05-07,184
-                2020-05-08,244
-                2020-05-09,280
-                2020-05-10,324
-                2020-05-11,387
-                2020-05-12,491
-                2020-05-13,645
-                2020-05-14,925'''
+dates = ['01-02-2020', '02-02-2020', '03-03-2020']
+prices = [1.4, 2.5, 9.6]
 
-sample_list = [ {'x': conv(1588745371), 'y': 400},
-                {'x': conv(1588845371), 'y': 500},
-                {'x': conv(1588946171), 'y': 800}]
-
-def csv_to_points(data):
-    """ Convert CSV string to list of dictionaries compatible with chart data obj """
-    data = data.split('\n')
-    data = [d.split(',') for d in data]
-    return [ dict(x = d[0], y = d[1]) for d in data]
+sample_list =[dict(x=conv(date), y=prices[idx]) for idx,date in enumerate(dates)]
 
 app = Flask(__name__)
 
@@ -43,32 +28,6 @@ def data():
     d = {'datasets':
             [
                 {'title': 'From Dict',
-                 'data': sample_list,
-                },
-
-                {'title': 'From CSV',
-                 'data': csv_to_points(sample_csv)
-                }
-            ],
-        }
-
-    return jsonify(d)
-
-@app.route('/dynamic')
-def dynamic():
-    """ This route is an example of receiving data from the frontend """
-
-    q = request.args.get('q')
-
-    print (q)
-
-    # You could dynamically build the following result now
-    # based on the value of `q` which is passed from the frontend.
-    # Here we just set that to the title.
-
-    d = {'datasets':
-            [
-                {'title': q,
                  'data': sample_list,
                 },
             ],
