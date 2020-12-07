1dfe659f6a6d703069150cc12b6f7e5947fdff52
v25 <>
2020-12-07 20:52:23 +0000

    cleanup

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, jsonify, url_for, request
 import datetime as DT
 
 def conv(date_str):
-    """ helper function convers epoch to iso format compatible with moment """
+    """ helper function convers date_str to iso format compatible with moment """
     return DT.datetime.strptime(date_str, '%m-%d-%Y').isoformat()
 
 dates = ['01-02-2020', '02-02-2020', '03-03-2020']
@@ -11,6 +11,8 @@ prices = [1.4, 2.5, 9.6]
 
 sample_list =[dict(x=conv(date), y=prices[idx]) for idx,date in enumerate(dates)]
 
+print( sample_list)
+exit()
 app = Flask(__name__)
 
 # Disable caching of staic assets; useful for dev
